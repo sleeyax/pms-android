a2b5842ab8081932b9a5f6982a55083d6e0bed4d
Sleeyax <yourd3veloper@gmail.com>
2020-08-16 14:05:34 +0200

    Fix log line format

--- src/helpers.js
+++ src/helpers.js
@@ -5,9 +5,8 @@ const appendFileAsyncPromise = promisify(fs.appendFile)
 
 module.exports = {
   writeToLog: (msg, logDir, isError = false) => {
-    // TODO: figure out if this is the right location
     const date = new Date()
-    const timestamp = date.toDateString().split(' ')
+    const timestamp = date.toTimeString().split(' ')
     const dateTimeStamp = `${date.toDateString()} ${timestamp}`
     return appendFileAsyncPromise(path.join(logDir, 'log.txt'), `[${dateTimeStamp}] ${isError ? 'error' : 'info'}: ${msg}\n`);
   }
